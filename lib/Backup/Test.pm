02d7ab9a888dfd78e2adea9f4fcbb3ac0640dbe9
Jozef Reisinger <jozef.reisinger@gmail.com>
2011-11-12 12:47:48 +0100

    Create basic structure

---
+++ lib/Backup/Test.pm
@@ -0,0 +1,43 @@
+package Math::Base4;
+
+use warnings;
+use strict;
+
+our $VERSION = '0.010';
+
+use Exporter 'import';
+our @EXPORT_OK = qw(add4);
+
+sub add4 {
+  my $first  = shift;
+  my $second = shift;
+  
+  return $first + $second;
+}
+
+
+1;
+
+
+__END__
+
+=head1 NAME
+
+Math::Base4 - Base 4 calculations
+
+=head1 SYNOPSIS
+
+  use Math::Base4 qw(add4);
+  
+  print add4(2,2);
+  # 10
+
+=head1 DESCRIPTION
+
+FIXME please
+
+=head1 AUTHOR
+
+BA.pm
+
+=cut
