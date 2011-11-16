c36c1996a7b626fc7a9acb8c17c7a472aa92f194
Jozef Reisinger <jozef.reisinger@gmail.com>
2011-11-16 12:39:37 +0100

    mail.pl working ok

--- main.pl
+++ main.pl
@@ -4,19 +4,33 @@ use warnings;
 use File::Basename;
 use Backup::Test;
 
+my $verbose = 1;  # FIXME - make verbose command line option
+
 my $dir = '/var/tmp/x';
-my $backup_dir = '/tmp/backup/x';
+my $backup_dir = '/var/tmp/backup/x';
 my @returned = Backup::Test::get_files($dir, 0.1);
 my $total_files = shift @returned;
 my @files = @returned;
 
+my $checked = 0;
+my $ok = 0;
+my $not_ok = 0;
 for my $file ( @files ) {
-  my $backup_file = basename $file;
-  unless ( Backup::Test::gen_md5sum($file) eq Backup::Test::gen_md5sum("$backup_dir/$backup_file") ) {
+  print "Working on: $file\n" if $verbose;
+  my($filename, $directories, $suffix) = fileparse $file; # $file has file with full path
+  my $backup_directories = $directories;
+  $backup_directories =~ s#$dir#$backup_dir#;
+  # FIXME: make '/' more portable
+  if ( Backup::Test::gen_md5sum("$directories/$filename") eq Backup::Test::gen_md5sum("$backup_directories/$filename") ) {
+    $ok++;
+  } else {
     print "File '$file' not backed up properly\n";
+    $not_ok++;
   }
+  $checked++;
 }
 
-print "Total files: $total_files\n";
-
-print "$_ ", Backup::Test::gen_md5sum($_), "\n" for @files;
+print "-" x 70, "\n";
+print "Checked $checked files out of $total_files total files\n";
+print "Backed up correctly: $ok\n";
+print "Backed up in-correctly: $not_ok\n";
